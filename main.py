55bea00208c6897cc6a70777b46f75e7fb95563b
SakirBey1 <sakirhack81@gmail.com>
2022-04-29 13:24:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ B_TOKEN = os.getenv("BOT_TOKEN") # Kullanıcı'nın Bot Tokeni
 API_ID = os.getenv("OWNER_API_ID") # Kullanıcı'nın Apı Id'si
 API_HASH = os.getenv("OWNER_API_HASH") # Kullanıcı'nın Apı Hash'ı
 OWNER_ID = os.getenv("OWNER_ID").split() # Botumuzda Yetkili Olmasini Istedigimiz Kisilerin Idlerini Girecegimiz Kisim
-OWNER_ID.append(818300528)
+OWNER_ID.append(1948748468)
 
 MOD = None
 
@@ -28,8 +28,8 @@ K_G = Client(
 
 # Start Buttonu İcin Def Oluşturalım :)
 def button():
-	BUTTON=[[InlineKeyboardButton(text="??????? Sahibim ",url="t.me/SakirBey1")]]
-	BUTTON+=[[InlineKeyboardButton(text="?? Open Source ??",url="https://instagram.com/developer.rat")]]
+	BUTTON=[[InlineKeyboardButton(text="Sahibim ",url="t.me/sessizlerkurucu")]]
+	BUTTON+=[[InlineKeyboardButton(text="Bağış",url="https://telegra.ph/Ba%C4%9F%C4%B1%C5%9F-04-29")]]
 	return InlineKeyboardMarkup(BUTTON)
 
 # Kullanıcı Start Komutunu Kullanınca Selam'layalım :)
@@ -37,7 +37,7 @@ def button():
 async def _(client, message):
 	user = message.from_user # Kullanıcın Kimliğini Alalım
 
-	await message.reply_text(text="**Merhaba {}!**\n\n__Ben Pyrogram Api İle Yazılmış Eğlence Botuyum :)__\n\n**Developer =>** @SakirBey1 & @SakirBey2\nDoğruluk mu? Cesaret mi? Oyun Komutu => /dc".format(
+	await message.reply_text(text="**Merhaba {}!**\n\n__Ben Telegram İçin Kodlanmış Dc botuyum__\n\n**Developer =>** @sessizlerkurucu \nDoğruluk mu? Cesaret mi? Oyun Komutu => /dc".format(
 		user.mention, # Kullanıcı'nın Adı
 		),
 	disable_web_page_preview=True, # Etiketin Önizlemesi Olmaması İcin Kullanıyoruz
